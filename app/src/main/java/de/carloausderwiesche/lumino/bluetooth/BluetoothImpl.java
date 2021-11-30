8d71f0813bd47d2129c4cbf726224ef111d67f5c
carloausderwiesche <carlo.ausderwiesche@htw-berlin.de>
2021-11-30 14:53:58 +0100

    created BluetoothImpl class

---
+++ app/src/main/java/de/carloausderwiesche/lumino/bluetooth/BluetoothImpl.java
@@ -0,0 +1,8 @@
+package de.carloausderwiesche.lumino.bluetooth;
+
+public class BluetoothImpl implements IBluetooth {
+    @Override
+    public boolean enableBluetooth() {
+        return false;
+    }
+}
