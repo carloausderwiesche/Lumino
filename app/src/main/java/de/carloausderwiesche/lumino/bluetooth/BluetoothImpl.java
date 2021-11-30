c7b213d46c068bb865206c7dc4b10ad768ae59c7
carloausderwiesche <carlo.ausderwiesche@htw-berlin.de>
2021-11-30 19:26:05 +0100

    created BluetoothImpl class

--- app/src/main/java/de/carloausderwiesche/lumino/bluetooth/BluetoothImpl.java
+++ app/src/main/java/de/carloausderwiesche/lumino/bluetooth/BluetoothImpl.java
@@ -1,8 +1,29 @@
 package de.carloausderwiesche.lumino.bluetooth;
 
+import android.bluetooth.BluetoothAdapter;
+import android.widget.Toast;
+
 public class BluetoothImpl implements IBluetooth {
+    private BluetoothAdapter btAdapter;
+
+    public BluetoothImpl(){
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+    }
+
     @Override
     public boolean enableBluetooth() {
-        return false;
+        if (btAdapter == null){
+            //TODO: Toast pop up
+            //Toast.makeText(this,"Bluetooth not supported!", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if (btAdapter.isEnabled()) return true;
+
+
+        return  true;
     }
+
+
+
 }
