72b0925d6d911d98c4aa6e536986dc6e2f8dca39
carloausderwiesche <carlo.ausderwiesche@htw-berlin.de>
2021-12-01 17:06:03 +0100

    added ClientActivity added button hostSession to HostActivity

--- app/src/main/java/de/carloausderwiesche/lumino/MainActivity.java
+++ app/src/main/java/de/carloausderwiesche/lumino/MainActivity.java
@@ -2,13 +2,30 @@ package de.carloausderwiesche.lumino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn_hostSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_hostSession = findViewById(R.id.btn_hostSession);
+        btn_hostSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityHost();
+            }
+        });
+    }
+
+    private void openActivityHost() {
+        Intent intent = new Intent(this, HostActivity.class);
+        startActivity(intent);
     }
 }
