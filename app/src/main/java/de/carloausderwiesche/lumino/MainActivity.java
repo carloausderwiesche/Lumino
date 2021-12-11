b698b957e892cac41ae9be54c03b792f49f43ff6
carloausderwiesche <carlo.ausderwiesche@htw-berlin.de>
2021-12-11 14:42:06 +0100

    11.12.21

--- app/src/main/java/de/carloausderwiesche/lumino/MainActivity.java
+++ app/src/main/java/de/carloausderwiesche/lumino/MainActivity.java
@@ -2,25 +2,32 @@ package de.carloausderwiesche.lumino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import de.carloausderwiesche.lumino.bluetooth.BluetoothImpl;
+
 public class MainActivity extends AppCompatActivity {
     private Button btnHostSession;
     private Button btnJoinSession;
+    private static Context appContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        appContext = getApplicationContext();
+
         btnHostSession = findViewById(R.id.btn_hostSession);
         btnHostSession.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivityHost();
+                startBluetoothHost();
             }
         });
 
@@ -34,13 +41,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void openActivityClient() {
-        Intent intent = new Intent(this, ClientActivity.class);
-        startActivity(intent);
+    public static Context getAppContext(){
+        return appContext;
     }
 
     private void openActivityHost() {
         Intent intent = new Intent(this, HostActivity.class);
         startActivity(intent);
+    }
+
+    private void openActivityClient() {
+        Intent intent = new Intent(this, ClientActivity.class);
+        startActivity(intent);
+    }
+
+    private void startBluetoothHost() {
+        BluetoothImpl bt = new BluetoothImpl();
+
     }
 }
--- app/src/main/java/de/carloausderwiesche/lumino/bluetooth/BluetoothImpl.java
+++ app/src/main/java/de/carloausderwiesche/lumino/bluetooth/BluetoothImpl.java
@@ -1,7 +1,10 @@
 package de.carloausderwiesche.lumino.bluetooth;
 
 import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.widget.Toast;
+
+import de.carloausderwiesche.lumino.MainActivity;
 
 public class BluetoothImpl implements IBluetooth {
     private BluetoothAdapter btAdapter;
@@ -13,10 +16,9 @@ public class BluetoothImpl implements IBluetooth {
     @Override
     public boolean enableBluetooth() {
         if (btAdapter == null){
-            //TODO: Toast pop up
-            //Toast.makeText(this,"Bluetooth not supported!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.getAppContext(), "No bluetooth adapter available", Toast.LENGTH_SHORT).show();
             return false;
-        }
+        } else Toast.makeText(MainActivity.getAppContext(), "Bluetooth adapter available", Toast.LENGTH_SHORT).show();
 
         if (btAdapter.isEnabled()) return true;
 
