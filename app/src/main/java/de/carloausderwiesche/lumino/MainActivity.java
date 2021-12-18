17ac6b9e339dda27f92919204b3596d6a35be5e0
carloausderwiesche <carlo.ausderwiesche@htw-berlin.de>
2021-12-18 11:27:29 +0100

    add flash component

--- app/src/main/java/de/carloausderwiesche/lumino/MainActivity.java
+++ app/src/main/java/de/carloausderwiesche/lumino/MainActivity.java
@@ -1,23 +1,20 @@
 package de.carloausderwiesche.lumino;
 
-import static androidx.core.app.ActivityCompat.startActivityForResult;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
-import de.carloausderwiesche.lumino.bluetooth.BluetoothImpl;
+import de.carloausderwiesche.lumino.controller.flash.Flash;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnHostSession;
     private Button btnJoinSession;
     private static Context appContext;
+    private static Object systemCameraService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         appContext = getApplicationContext();
+        systemCameraService = getSystemService(CAMERA_SERVICE);
 
         btnHostSession = findViewById(R.id.btn_hostSession);
         btnHostSession.setOnClickListener(v -> {
             openActivityHost();
+            Flash flash = Flash.getFlashComponent();
+            flash.turnFlashOn();
         });
 
         btnJoinSession = findViewById(R.id.btn_joinSession);
@@ -41,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
     public static Context getAppContext(){
         return appContext;
+    }
+
+    public static Object getSystemCameraService() {
+        return systemCameraService;
     }
 
     private void openActivityHost() {
---
+++ app/src/main/java/de/carloausderwiesche/lumino/controller/flash/Flash.java
@@ -0,0 +1,58 @@
+package de.carloausderwiesche.lumino.controller.flash;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.widget.Switch;
+import android.widget.Toast;
+
+import de.carloausderwiesche.lumino.MainActivity;
+
+public class Flash {
+
+    private static Flash singleton = null;
+    CameraManager cameraManager;
+
+    private Flash(){
+        cameraManager = (CameraManager) MainActivity.getSystemCameraService();
+
+        if (MainActivity.getAppContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY)){
+            if (MainActivity.getAppContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)){
+                Toast.makeText(MainActivity.getAppContext(), "This device has flash", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.getAppContext(), "This device has no flash", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(MainActivity.getAppContext(), "This device has no camera", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public static Flash getFlashComponent(){
+        if (Flash.singleton == null){
+            Flash.singleton = new Flash();
+        }
+        return Flash.singleton;
+    }
+
+    public void turnFlashOn(){
+        try {
+            cameraManager.setTorchMode("0", true);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void turnFlashOff(){
+        try {
+            cameraManager.setTorchMode("0", false);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
