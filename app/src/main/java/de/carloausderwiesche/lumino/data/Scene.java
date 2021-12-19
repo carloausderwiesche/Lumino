3db362170ceefc7149c4eb8fc2672b05c8395b37
carloausderwiesche <carlo.ausderwiesche@htw-berlin.de>
2021-12-19 19:13:44 +0100

    added Room and made Scene.java as entity for the database

--- app/src/main/java/de/carloausderwiesche/lumino/data/Scene.java
+++ app/src/main/java/de/carloausderwiesche/lumino/data/Scene.java
@@ -1,27 +1,57 @@
 package de.carloausderwiesche.lumino.data;
 
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
 import java.util.LinkedList;
 
+@Entity(tableName = "scene_table")
 public class Scene {
+    @Ignore
     private static Scene singleton = null;
-    private int ID;
-    private String name;
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String titel;
     private String caption;
     private String pattern;
     private long delay;
 
-    private Scene(){
-        pattern = "10101010101";
-        delay = 500L;
+    public Scene(String titel, String caption, String pattern, long delay) {
+        this.titel = titel;
+        this.caption = caption;
+        this.pattern = pattern;
+        this.delay = delay;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public static Scene getSceneComponent(){
         if (Scene.singleton == null){
-            Scene.singleton = new Scene();
+          //  Scene.singleton = new Scene();
         }
         return Scene.singleton;
     }
 
+    public static Scene getSingleton() {
+        return singleton;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitel() {
+        return titel;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
 
     public String getPattern() {
         return pattern;
